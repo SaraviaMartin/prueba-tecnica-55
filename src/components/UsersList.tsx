4c46990a495d0abdf2f089008f3cbf7ab18d5425
Juan <saraviamartin16@hotmail.com>
2023-10-16 15:28:24 -0300

    table color with css

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -18,13 +18,11 @@ export  function UsersList({deleteUser, showColors, users}: Props) {
             </tr>
         </thead>
 
-        <tbody>
+        <tbody className={showColors ? 'table--showColors' : 'table'}>
             {
-                users.map((user, index) => {
-                    const backgroundColor = index % 2 === 0  ? '#333' : '#555'
-                    const color = showColors ? backgroundColor : 'transparent'
+                users.map((user) => {
                     return (
-                        <tr key={user.email} style={{backgroundColor: color}}>
+                        <tr key={user.email} >
                             <td>
                                 <img src={user.picture.thumbnail} />
                             </td>
